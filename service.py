962249d737b44f7ddc552ef8ffc59cb879d00c4d
Anthony Cagliano <acagliano97@gmail.com>
2023-10-01 23:20:22 -0400

    tls and rest of packet handling

--- service.py
+++ service.py
@@ -1,4 +1,4 @@
-import socket, json, threading, sys, time, logging, os, asyncio
+import socket, json, threading, sys, time, logging, os, asyncio, ssl
 import logging.handlers
 
 try:
@@ -12,8 +12,10 @@ calc_connected = False
 dev_serial = None
 dev_socket = None
 dev_socket_tls = None
+tls_context = ssl.create_default_context()
 logservice = None
 server_connected = False
+server_hostname = None
 TI84PCE_IDENTIFIER = (0x16C0, 0x05E1)
 PACKET_MAX_SIZE = 2046
 
@@ -26,18 +28,21 @@ FunctionCodes = {
 }
 
 def u24(*args):
-	o=[]
-	for arg in args:
-		if int(arg)<0: arg = abs(int(arg))
-		else: arg = int(arg)
-		o.extend(list(int(arg).to_bytes(3,'little')))
-	return o
+    o=[]
+    for arg in args:
+        if int(arg)<0: arg = abs(int(arg))
+        else: arg = int(arg)
+        o.extend(list(int(arg).to_bytes(3,'little')))
+    return o
 
 def tcp_recv():
     global service_active, server_connected
-    while server_connected and service_active:
+    while server_connected and calc_connected and service_active:
         try:
-            status = dev_serial.write(dev_serial.recv(PACKET_MAX_SIZE))
+            if dev_socket_tls == None:
+                status = dev_serial.write(dev_socket.recv(PACKET_MAX_SIZE))
+            else:
+                status = dev_serial.write(dev_socket_tls.recv(PACKET_MAX_SIZE))
         except socket.error as e:
             logservice.log(logging.ERROR, "{}\n".format(e))
 
@@ -58,11 +63,12 @@ def relay():
                         continue
                     if data[0]==ControlCode:
                         if data[1]==FunctionCodes["CONNECT"]:
+                            global server_hostname
                             if server_connected:
                                 logservice.log(logging.ERROR, "Already connected to a server. Disconnect first.")
                             try:
                                 hostinfo=str(data[2:], 'utf-8').split("\0")
-                                server=hostinfo[0]
+                                server=server_hostname=hostinfo[0]
                                 port=hostinfo[1]
                                 dev_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                                 dev_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -74,6 +80,22 @@ def relay():
                         elif data[1]==FunctionCodes["DISCONNECT"]:
                             dev_socket.shutdown()
                             server_connected = False
+                        elif data[1]==FunctionCodes["STARTTLS"]:
+                            global dev_socket_tls
+                            dev_socket_tls = tls_context.wrap_socket(dev_socket, server_hostname=server_hostname)
+                        elif data[1]==FunctionCodes["ENDTLS"]:
+                            global dev_socket_tls
+                            dev_socket_tls = None
+                        else:
+                            logservice.log(logging.ERROR, "Invalid bridge function code. Ignoring packet.")
+                    else:
+                        if not(server_connected):
+                            logservice.log(logging.ERROR, "Cannot relay--not connected to a server")
+                            continue
+                        if dev_socket_tls == None:
+                            dev_socket.send(bytes(u24(size)) + data)
+                        else:
+                            dev_socket_tls.send(bytes(u24(size)) + data)
         except IOError:
             logservice.log(logging.INFO, "Serial device closed. Returning to listen mode.")
             calc_connected = False
