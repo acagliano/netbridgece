487a2f395d28ed9e09e0cd9d0787d40a8859d61b
Anthony Cagliano <acagliano97@gmail.com>
2023-10-03 12:33:01 -0400

    reorganize some of the variables

--- service.py
+++ service.py
@@ -8,23 +8,27 @@ except ImportError:
     exit(1)
 
 service_active = True
-calc_connected = False
-dev_serial = None
-dev_socket = None
-dev_socket_tls = None
 tls_context = ssl.create_default_context()
+
+calc = {
+    "serial": None
+}
+remote = {
+    "host": None,
+    "socket": None,
+    "socket_tls": None
+}
 logservice = None
-server_connected = False
-server_hostname = None
 TI84PCE_IDENTIFIER = (0x16C0, 0x05E1)
 PACKET_MAX_SIZE = 2046
 
 ControlCode = 0xbc
 FunctionCodes = {
-    "CONNECT": 0x00,
-    "DISCONNECT": 0x01,
-    "STARTTLS": 0x02,
-    "ENDTLS": 0x03
+    "PING": 0x00,
+    "CONNECT": 0x01,
+    "DISCONNECT": 0x02,
+    "STARTTLS": 0x03,
+    "ENDTLS": 0x04
 }
 
 def u24(*args):
@@ -37,82 +41,93 @@ def u24(*args):
 
 def tcp_recv():
     global service_active, server_connected
-    while server_connected and calc_connected and service_active:
+    while calc["serial"] and remote["socket"] and service_active:
         try:
-            if dev_socket_tls == None:
-                status = dev_serial.write(dev_socket.recv(PACKET_MAX_SIZE))
+            if remote["socket_tls"]:
+                status = calc["serial"].write(remote["socket_tls"].recv(PACKET_MAX_SIZE))
             else:
-                status = dev_serial.write(dev_socket_tls.recv(PACKET_MAX_SIZE))
+                status = calc["serial"].write(remote["socket"].recv(PACKET_MAX_SIZE))
         except socket.error as e:
-            logservice.log(logging.ERROR, "{}\n".format(e))
+            logservice.log(logging.ERROR, e)
 
 def relay():
-    global dev_socket, calc_connected, server_connected
+    global calc, remote
     logservice.log(logging.INFO, "Service in relay mode...")
-    while calc_connected and service_active:
+    while calc["serial"] and service_active:
         try:
-            while True:
-                size = int.from_bytes(dev_serial.read(3), 'little')
-                if not(size) or (size > PACKET_MAX_SIZE):
-                    logservice.log(logging.ERROR, "Serial packet dropped, invalid size.")
+            size = int.from_bytes(calc["serial"].read(3), 'little')
+            if not(size) or (size > PACKET_MAX_SIZE):
+                logservice.log(logging.ERROR, "Serial packet dropped, invalid size.")
+                continue
+            else:
+                data = calc["serial"].read(size)
+                if len(data) != size:
+                    logservice.log(logging.ERROR, "Serial packet dropped, packet incomplete.")
                     continue
-                else:
-                    data = dev_serial.read(size)
-                    if len(data) != size:
-                        logservice.log(logging.ERROR, "Serial packet dropped, packet incomplete.")
-                        continue
-                    if data[0]==ControlCode:
-                        if data[1]==FunctionCodes["CONNECT"]:
-                            global server_hostname
-                            if server_connected:
-                                logservice.log(logging.ERROR, "Already connected to a server. Disconnect first.")
-                            try:
-                                hostinfo=str(data[2:], 'utf-8').split("\0")
-                                server=server_hostname=hostinfo[0]
-                                port=hostinfo[1]
-                                dev_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                                dev_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-                                dev_socket.connect((server, port))
-                                logservice.log(logging.INFO, "Serial bridged to {} on port {}.".format(server, port))
-                                server_connected = True
-                            except Exception as e:
-                                logservice.log(logging.ERROR, e)
-                        elif data[1]==FunctionCodes["DISCONNECT"]:
-                            dev_socket.shutdown()
-                            server_connected = False
-                        elif data[1]==FunctionCodes["STARTTLS"]:
-                            global dev_socket_tls
-                            dev_socket_tls = tls_context.wrap_socket(dev_socket, server_hostname=server_hostname)
-                        elif data[1]==FunctionCodes["ENDTLS"]:
-                            global dev_socket_tls
-                            dev_socket_tls = None
-                        else:
-                            logservice.log(logging.ERROR, "Invalid bridge function code. Ignoring packet.")
+                if data[0]==ControlCode:
+                    if data[1]==FunctionCodes["PING"]:
+                        calc["serial"].write(b'\x02\x00\x00\xBC\x00')
+                    elif data[1]==FunctionCodes["CONNECT"]:
+                        try:
+                            if remote["socket"]:
+                                logservice.log(logging.ERROR, "Already connected to a server. Disconnecting.")
+                                remote["socket"].shutdown()
+                            hostinfo=str(data[2:], 'utf-8').split("\0")
+                            hostname = hostinfo[0]
+                            port = int(hostinfo[1])
+                            remote["socket"] = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                            remote["socket"].setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                            remote["socket"].connect((hostname, port))
+                            logservice.log(logging.INFO, "Serial bridged to {} on port {}.".format(hostname, port))
+                            remote["host"] = (hostname, port)
+                            calc["serial"].write(b'\x03\x00\x00\xBC\x01\x00')
+                        except Exception as e:
+                            logservice.log(logging.ERROR, e)
+                            calc["serial"].write(b'\x03\x00\x00\xBC\x01\x00')
+                    elif data[1]==FunctionCodes["DISCONNECT"]:
+                        remote["socket"].shutdown()
+                        remote["socket"].close()
+                        remote["socket"] = None
+                        remote["host"] = None
+                    elif data[1]==FunctionCodes["STARTTLS"]:
+                        remote["socket_tls"] = tls_context.wrap_socket(dev_socket, server_hostname=remote["host"][0])
+                    elif data[1]==FunctionCodes["ENDTLS"]:
+                        remote["socket_tls"] = None
                     else:
-                        if not(server_connected):
-                            logservice.log(logging.ERROR, "Cannot relay--not connected to a server")
-                            continue
-                        if dev_socket_tls == None:
-                            dev_socket.send(bytes(u24(size)) + data)
-                        else:
-                            dev_socket_tls.send(bytes(u24(size)) + data)
+                        logservice.log(logging.ERROR, "Invalid bridge function code {}. Ignoring packet.".format(data[1]))
+                else:
+                    if remote["socket"] == None:
+                        logservice.log(logging.ERROR, "Cannot relay--not connected to a server")
+                        continue
+                    if remote["socket_tls"]:
+                        remote["socket_tls"].send(bytes(u24(size)) + data)
+                    else:
+                        remote["socket"].send(bytes(u24(size)) + data)
         except IOError:
             logservice.log(logging.INFO, "Serial device closed. Returning to listen mode.")
-            calc_connected = False
-            dev_socket.shutdown()
+            calc["serial"].reset_input_buffer()
+            calc["serial"].reset_output_buffer()
+            calc["serial"].close()
+            if remote["tls_socket"]:
+                remote["tls_socket"].shutdown()
+                remote["tls_socket"].close()
+                remote["tls_socket"] = None
+            if remote["socket"]:
+                remote["socket"].shutdown()
+                remote["socket"].close()
+                remote["socket"] = None
             return
     return
 
 def listen():
-    global dev_serial, calc_connected, logservice
+    global calc, logservice
     logservice.log(logging.INFO, "Service in listen mode...")
     while service_active:
         ports = [x for x in serial.tools.list_ports.comports(include_links=True) if (x.vid, x.pid) == TI84PCE_IDENTIFIER]
         if len(ports):
             serial_name = ports[0].device
             logservice.log(logging.INFO, "Calculator has been detected at {}.".format(serial_name))
-            dev_serial = serial.Serial(serial_name, timeout=None)
-            calc_connected = True
+            calc["serial"] = serial.Serial(serial_name, timeout=None)
             relay()
         time.sleep(0.5)
 
